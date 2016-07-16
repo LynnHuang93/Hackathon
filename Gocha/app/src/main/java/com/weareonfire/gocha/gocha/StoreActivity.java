95045fafdc8aedd488169878338c296f019b3175
Xue <xiaosxue@amazon.com>
2016-07-16 08:07:53 -0700

    added files

--- Gocha/app/src/main/java/com/weareonfire/gocha/gocha/StoreActivity.java
+++ Gocha/app/src/main/java/com/weareonfire/gocha/gocha/StoreActivity.java
@@ -2,8 +2,10 @@ package com.weareonfire.gocha.gocha;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 public class StoreActivity extends AppCompatActivity {
     Context context = this;
@@ -13,6 +15,25 @@ public class StoreActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_store);
         final SharedPreferences sharedPref = context.getSharedPreferences("preferences", Context.MODE_PRIVATE);
-        int coins = sharedPref.getInt("coins", 0);
+        final SharedPreferences.Editor editor = sharedPref.edit();
+        final int coins = sharedPref.getInt("coins", 0);
+        ImageView pearlMinus = (ImageView) findViewById(R.id.exemptMinus);
+        pearlMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (coins>=5){
+                    editor.putInt("coins",coins-5);
+                }
+            }
+        });
+        ImageView pearlPlus = (ImageView) findViewById(R.id.exemptPlus);
+        pearlMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (coins>=5){
+                    editor.putInt("coins",coins-5);
+                }
+            }
+        });
     }
 }
